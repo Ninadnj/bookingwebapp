53ad7e1f266b9a5b222ed04c81a59bb700ea8351
Nino Doinjashvili <ninodoinjashvili@gmail.com>
2025-12-17 02:53:10 +0100

    Fix: Switch to HashRouter for GitHub Pages support

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { ClientHome } from './pages/ClientHome'
 import { AdminDashboard } from './pages/AdminDashboard'
 
